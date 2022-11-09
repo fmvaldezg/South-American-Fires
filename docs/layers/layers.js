c55fb53f841047664148f114f869daa0410f94eb
Felipe Valdez <fmvaldezg@yahoo.com>
2022-11-09 16:49:12 -0500

    changed files

--- docs/layers/layers.js
+++ docs/layers/layers.js
@@ -1,5 +1,3 @@
-ol.proj.proj4.register(proj4);
-ol.proj.get("EPSG:4326").setExtent([-116.373023, -58.753547, 2.717406, 19.828092]);
 var wms_layers = [];
 
 
@@ -14,7 +12,7 @@ var wms_layers = [];
                 url: 'http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
             })
         });
-var lyr_VIIRSSuomiNPP72hrsfireshotspots_1 = new ol.layer.Tile({
+var lyr_72hrsfireshotspotsVIIRS_1 = new ol.layer.Tile({
                             source: new ol.source.TileWMS(({
                               url: "https://firms.modaps.eosdis.nasa.gov/mapserver/wms/fires/60b625c71ff732afb4bb397003ab597c/",
     attributions: ' ',
@@ -23,32 +21,32 @@ var lyr_VIIRSSuomiNPP72hrsfireshotspots_1 = new ol.layer.Tile({
                                 "TILED": "true",
                                 "VERSION": "1.1.1"},
                             })),
-                            title: "VIIRS Suomi NPP 72 hrs fires/hotspots",
+                            title: "72 hrs fires/hotspots VIIRS",
                             opacity: 1.000000,
                             
                             
                           });
-              wms_layers.push([lyr_VIIRSSuomiNPP72hrsfireshotspots_1, 0]);
-var format_map_2 = new ol.format.GeoJSON();
-var features_map_2 = format_map_2.readFeatures(json_map_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
-var jsonSource_map_2 = new ol.source.Vector({
+              wms_layers.push([lyr_72hrsfireshotspotsVIIRS_1, 0]);
+var format_IndigenousTerritories_2 = new ol.format.GeoJSON();
+var features_IndigenousTerritories_2 = format_IndigenousTerritories_2.readFeatures(json_IndigenousTerritories_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_IndigenousTerritories_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_map_2.addFeatures(features_map_2);
-var lyr_map_2 = new ol.layer.Vector({
+jsonSource_IndigenousTerritories_2.addFeatures(features_IndigenousTerritories_2);
+var lyr_IndigenousTerritories_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_map_2, 
-                style: style_map_2,
+                source:jsonSource_IndigenousTerritories_2, 
+                style: style_IndigenousTerritories_2,
                 interactive: true,
-                title: '<img src="styles/legend/map_2.png" /> map'
+                title: '<img src="styles/legend/IndigenousTerritories_2.png" /> Indigenous Territories'
             });
 
-lyr_ESRIGraydark_0.setVisible(true);lyr_VIIRSSuomiNPP72hrsfireshotspots_1.setVisible(true);lyr_map_2.setVisible(true);
-var layersList = [lyr_ESRIGraydark_0,lyr_VIIRSSuomiNPP72hrsfireshotspots_1,lyr_map_2];
-lyr_map_2.set('fieldAliases', {'id': 'id', 'Name': 'Name', 'ID': 'ID', 'Slug': 'Slug', 'description': 'description', 'color': 'color', });
-lyr_map_2.set('fieldImages', {'id': 'TextEdit', 'Name': 'TextEdit', 'ID': 'Range', 'Slug': 'TextEdit', 'description': 'TextEdit', 'color': 'TextEdit', });
-lyr_map_2.set('fieldLabels', {'id': 'no label', 'Name': 'inline label', 'ID': 'no label', 'Slug': 'no label', 'description': 'inline label', 'color': 'no label', });
-lyr_map_2.on('precompose', function(evt) {
+lyr_ESRIGraydark_0.setVisible(true);lyr_72hrsfireshotspotsVIIRS_1.setVisible(true);lyr_IndigenousTerritories_2.setVisible(true);
+var layersList = [lyr_ESRIGraydark_0,lyr_72hrsfireshotspotsVIIRS_1,lyr_IndigenousTerritories_2];
+lyr_IndigenousTerritories_2.set('fieldAliases', {'id': 'id', 'Name': 'Name', 'Slug': 'Slug', 'description': 'description', 'color': 'color', });
+lyr_IndigenousTerritories_2.set('fieldImages', {'id': 'TextEdit', 'Name': 'TextEdit', 'Slug': 'TextEdit', 'description': 'TextEdit', 'color': 'TextEdit', });
+lyr_IndigenousTerritories_2.set('fieldLabels', {'id': 'no label', 'Name': 'inline label', 'Slug': 'no label', 'description': 'inline label', 'color': 'no label', });
+lyr_IndigenousTerritories_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
